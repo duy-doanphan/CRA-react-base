baee959519e1f88e6594836f27cc17347bee4b1f
Phan Duy <phanduy@Phans-MacBook-Pro.local>
2023-05-10 19:33:23 +0700

    vid18

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -6,11 +6,24 @@ class MyComponent extends Component {
        age : 21,
        address: 'DN'
    }
+    handleClick = (e) => {
+        alert('ok')
+        console.log(e.target)
+    }
+    handleHover = () => {
+        console.log('hover me')
+    }
 
     render() {
+
+
         return (
             <div>
                 My name is {this.state.name} and age : {this.state.age}
+                <button
+                    onClick={this.handleClick}
+                    onMouseOver={this.handleHover}
+                >Click me</button>
             </div>
         );
     }
